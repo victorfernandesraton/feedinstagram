c468b230a824a7676791b49de545efe84a7bcf94
victorfernandesraton <vfbraton@gmail.com>
2020-11-21 01:29:35 -0300

    identaçao

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -1,51 +1,51 @@
-
-import React, { useEffect, useState } from 'react';
-import { Animated } from 'react-native';
-import { Small, Original } from './styles';
+import React, { useEffect, useState } from "react";
+import { Animated } from "react-native";
+import { Small, Original } from "./styles";
 
 const AnimatedOriginal = Animated.createAnimatedComponent(Original);
 
 export default function LazyImage({
-  smallSource,
-  source,
-  shouldLoad = false,
-  aspectRatio = 1,
+	smallSource,
+	source,
+	shouldLoad = false,
+	aspectRatio = 1,
 }) {
-  const opacity = new Animated.Value(0);
+	const opacity = new Animated.Value(0);
 
-  const [loaded, setLoaded] = useState(false);
-  
-  useEffect(() => {
-    if (shouldLoad) {
-      setTimeout(() => {
-        setLoaded(true);
-      }, 1000);
-    }
-  }, [shouldLoad])
+	const [loaded, setLoaded] = useState(false);
 
-  function handleAnimate() {
-    Animated.timing(opacity, {
-      duration: 500,
-      toValue: 1,
-      useNativeDriver: true, 
-    }).start();
-  }
+	useEffect(() => {
+		if (shouldLoad) {
+			setTimeout(() => {
+				setLoaded(true);
+			}, 1000);
+		}
+	}, [shouldLoad]);
 
-  return (
-    <Small 
-      source={smallSource}
-      aspect={aspectRatio}
-      resizeMode="contain"
-      blurRadius={3}>
-      { loaded && (
-        <AnimatedOriginal 
-          style={{ opacity }}
-          onLoadEnd={handleAnimate}
-          source={source} 
-          aspect={aspectRatio} 
-          resizeMode="contain"
-        />
-      ) }
-    </Small>
-  );
+	function handleAnimate() {
+		Animated.timing(opacity, {
+			duration: 500,
+			toValue: 1,
+			useNativeDriver: true,
+		}).start();
+	}
+
+	return (
+		<Small
+			source={smallSource}
+			aspect={aspectRatio}
+			resizeMode="contain"
+			blurRadius={3}
+		>
+			{loaded && (
+				<AnimatedOriginal
+					style={{ opacity }}
+					onLoadEnd={handleAnimate}
+					source={source}
+					aspect={aspectRatio}
+					resizeMode="contain"
+				/>
+			)}
+		</Small>
+	);
 }
--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -1,38 +1,36 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
-export const Container = styled.View`
-  
-`;
+export const Container = styled.View``;
 
 export const Post = styled.View`
-  margin-top: 10px;
+	margin-top: 10px;
 `;
 
 export const Header = styled.View`
-  padding: 15px;
-  flex-direction: row;
-  align-items: center;
+	padding: 15px;
+	flex-direction: row;
+	align-items: center;
 `;
 
 export const Avatar = styled.Image`
-  width: 32px;
-  height: 32px;
-  border-radius: 16px;
-  margin-right: 10px;
+	width: 32px;
+	height: 32px;
+	border-radius: 16px;
+	margin-right: 10px;
 `;
 
 export const Name = styled.Text`
-  font-weight: 600;
+	font-weight: 600;
 `;
 
 export const Description = styled.Text`
-  padding: 15px;
-  line-height: 18px;
+	padding: 15px;
+	line-height: 18px;
 `;
 
 export const Loading = styled.ActivityIndicator.attrs({
-  size: 'small',
-  color: '#999'
+	size: "small",
+	color: "#999",
 })`
-  margin: 30px 0;
+	margin: 30px 0;
 `;
