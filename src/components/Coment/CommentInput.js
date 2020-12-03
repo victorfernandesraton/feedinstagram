7d0114c074d1a19eca91c20f7cf56547920c3b07
victorfernandesraton <vfbraton@gmail.com>
2020-12-02 21:45:09 -0300

    use authContext to post comment

--- src/components/Coment/CommentInput.js
+++ src/components/Coment/CommentInput.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useReducer, useState } from "react";
 
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useLogin } from "../login/Login-context";
 
 import { postComment } from "./Comment-action";
 import Reducer, { initialState } from "./Comment-reducer";
@@ -12,6 +13,7 @@ import {
 } from "./style";
 
 const CommentInput = ({ parent, onPost, total }) => {
+	const [{user}] = useLogin();
 	const [content, setContent] = useState("");
 	const [textHeight, setTextHeight] = useState(40);
 	const [{ loading }, dispatch] = useReducer(Reducer, initialState);
@@ -19,7 +21,7 @@ const CommentInput = ({ parent, onPost, total }) => {
 	const sendComment = useCallback(
 		async (content) => {
 			const response = await postComment(dispatch, {
-				author: { id: 1 },
+				author: { id: user.id },
 				content: content,
 				parent,
 			});
