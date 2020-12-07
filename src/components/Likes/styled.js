844a4aa9605811fe2ab9e38b2030dddb7bde3ab6
Maurilio David S. Conceicao <maurilio_david@hotmail.com>
2020-12-06 23:41:04 -0300

    Aplicando estilo like

---
+++ src/components/Likes/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    iconRow: {
+      flexDirection: "row",
+      alignSelf: "stretch",
+      marginTop: 10,
+      paddingVertical: 5,
+      paddingHorizontal: 15
+    },
+    heartIcon: {
+      width: 20,
+      height: 20
+    }
+});
